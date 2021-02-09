f8be07c9d08d140de8e0ce32bfe26f76653370de
Louis Young <me@louisyoung.co.uk>
2021-02-09 10:47:23 +0000

    Add Prism support

--- components/Content.js
+++ components/Content.js
@@ -1,8 +1,9 @@
 import styles from "./Content.module.css";
+import "prism-theme-night-owl";
 
 const Content = ({ content }) => {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-2xl mx-auto line-numbers">
       <div className={styles.markdown} dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
--- utilities/markdownToHtml.js
+++ utilities/markdownToHtml.js
@@ -1,8 +1,24 @@
 import remark from "remark";
 import html from "remark-html";
+import prism from "remark-prism";
 
 const markdownToHtml = async (markdown) => {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark()
+    .use(html)
+    .use(prism, {
+      plugins: [
+        "autolinker",
+        "command-line",
+        "data-uri-highlight",
+        "diff-highlight",
+        "inline-color",
+        "keep-markup",
+        "line-numbers",
+        "show-invisibles",
+        "treeview",
+      ],
+    })
+    .process(markdown);
 
   return result.toString();
 };
