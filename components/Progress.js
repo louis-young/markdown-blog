394e2cf2dfa8cd9a2b3d62ee29e7bd850bebbd9c
Louis Young <me@louisyoung.co.uk>
2021-02-10 16:50:10 +0000

    Add new blog

--- components/Progress.js
+++ components/Progress.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Progress.module.css";
 
 const Progress = () => {
   const [progress, setProgress] = useState(0);
 
-  const calculatePercentage = () => {
-    const scrolled = window.scrollY;
-
-    const total = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-    const percentage = (scrolled / total) * 100;
-
-    setProgress(percentage);
-  };
-
   useEffect(() => {
+    const calculatePercentage = () => {
+      const scrolled = window.scrollY;
+
+      const total = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+      const percentage = (scrolled / total) * 100;
+
+      setProgress(percentage);
+    };
+
     window.addEventListener("scroll", calculatePercentage);
     return () => {
       window.removeEventListener("scroll", calculatePercentage);
     };
-  }, [calculatePercentage]);
+  }, []);
 
-  return <progress className={styles.progress} value={progress} max="100"></progress>;
+  return <progress className={styles.progress} value={progress} max="100" />;
 };
 
 export default Progress;
