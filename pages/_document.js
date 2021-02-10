5ad9a292e362f087034d94cf6376db5a91dd22ab
Louis Young <me@louisyoung.co.uk>
2021-02-10 18:03:44 +0000

    Add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-CQ5X65RCG6"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-CQ5X65RCG6');`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
